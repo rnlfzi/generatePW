3a90bd53351c69e78ca8987974e6c1ae45f739a7
Reynald <rfaidzilah@gmail.com>
2024-01-16 10:10:58 +0700

    full content

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,174 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+
+import { BsCopy } from "react-icons/bs";
+import {
+  TbSquareRoundedCheck,
+  TbSquareRoundedCheckFilled,
+} from "react-icons/tb";
+import { GrPowerReset } from "react-icons/gr";
 
 export default function Home() {
+  const [password, setPassword] = useState("Im a password");
+  const [max, setMax] = useState(8);
+  const [value, setValue] = useState({
+    lowercase: false,
+    uppercase: false,
+    numbers: false,
+    symbols: false,
+  });
+
+  const handleSlider = (e: any) => {
+    const int = parseInt(e.target.value);
+    setMax(int);
+  };
+
+  const handleLower = () => setValue({ ...value, lowercase: !value.lowercase });
+  const handleUpper = () => setValue({ ...value, uppercase: !value.uppercase });
+  const handleNumbers = () => setValue({ ...value, numbers: !value.numbers });
+  const handleSymbols = () => setValue({ ...value, symbols: !value.symbols });
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(password)
+      .then(() => alert("Success copied to clipboard"))
+      .catch((err) => alert(err));
+  };
+
+  const handleReset = () => {
+    setValue({
+      lowercase: false,
+      uppercase: false,
+      numbers: false,
+      symbols: false,
+    });
+    setMax(8);
+    setPassword("Im a password");
+  };
+
+  const handleGenerate = () => {
+    let generatedPassword = "";
+
+    if (value.lowercase) {
+      generatedPassword += "abcdefghijklmnopqrstuvwxyz";
+    }
+    if (value.uppercase) {
+      generatedPassword += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if (value.numbers) {
+      generatedPassword += "0123456789";
+    }
+    if (value.symbols) {
+      generatedPassword += "!@#$%^&*()_+<>?/.,";
+    }
+
+    if (max < 1) return alert("Password length must be greater than 0");
+    if (max > 20) return alert("Password length must be less than 20");
+
+    let password = "";
+    for (let i = 0; i < max; i++) {
+      password += generatedPassword.charAt(
+        Math.floor(Math.random() * generatedPassword.length)
+      );
+    }
+
+    setPassword(password);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className=" w-full h-[100vh] bg-custom-main flex justify-center items-center flex-col gap-4">
+      <div className="max-w-lg bg-cyan-700 w-full p-4 rounded-lg">
+        <div className="flex justify-between gap-2">
+          <div className=" bg-cyan-500 text-white w-full px-4 py-3 rounded-lg">
+            <p>{password}</p>
+          </div>
+          <div className="flex gap-2">
+            <div
+              onClick={handleReset}
+              className=" bg-cyan-600 text-white p-4 rounded-full flex justify-center items-center cursor-pointer hover:bg-cyan-600/80"
+            >
+              <GrPowerReset />
+            </div>
+            <div
+              onClick={handleCopy}
+              className=" bg-cyan-600 text-white p-4 rounded-full flex justify-center items-center cursor-pointer hover:bg-cyan-600/80"
+            >
+              <BsCopy />
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="max-w-lg bg-cyan-700 w-full p-4 rounded-lg flex flex-col justify-center gap-4">
+        <div className="flex justify-between gap-4">
+          <div className=" bg-cyan-600 text-white px-5 py-3 rounded-lg flex justify-center items-center">
+            {max}
+          </div>
+          <div className=" bg-cyan-500 text-white w-full px-4 py-3 rounded-lg flex items-center">
+            <input
+              value={max}
+              type="range"
+              onChange={(e) => handleSlider(e)}
+              className="w-full"
+            />
+          </div>
+        </div>
+        <div className=" grid grid-cols-2 gap-4">
+          <div
+            onClick={handleLower}
+            className="flex px-4 py-3 w-full bg-cyan-900 rounded-lg items-center gap-2 text-white cursor-pointer"
+          >
+            {value.lowercase ? (
+              <TbSquareRoundedCheckFilled size={25} />
+            ) : (
+              <TbSquareRoundedCheck size={25} />
+            )}
+            <p>a-z</p>
+          </div>
+          <div
+            onClick={handleUpper}
+            className="flex px-4 py-3 w-full bg-cyan-900 rounded-lg items-center gap-2 text-white cursor-pointer"
+          >
+            {value.uppercase ? (
+              <TbSquareRoundedCheckFilled size={25} />
+            ) : (
+              <TbSquareRoundedCheck size={25} />
+            )}
+            <p>A-Z</p>
+          </div>
+          <div
+            onClick={handleNumbers}
+            className="flex px-4 py-3 w-full bg-cyan-900 rounded-lg items-center gap-2 text-white cursor-pointer"
+          >
+            {value.numbers ? (
+              <TbSquareRoundedCheckFilled size={25} />
+            ) : (
+              <TbSquareRoundedCheck size={25} />
+            )}
+            <p>0-9</p>
+          </div>
+          <div
+            onClick={handleSymbols}
+            className="flex px-4 py-3 w-full bg-cyan-900 rounded-lg items-center gap-2 text-white cursor-pointer"
+          >
+            {value.symbols ? (
+              <TbSquareRoundedCheckFilled size={25} />
+            ) : (
+              <TbSquareRoundedCheck size={25} />
+            )}
+            <p>!@#$%</p>
+          </div>
+        </div>
+        <p className="text-white text-xs italic">
+          * Silakan masukan format yang anda inginkan
+        </p>
+        <div
+          onClick={handleGenerate}
+          className="bg-cyan-600 px-4 py-3 rounded-lg cursor-pointer hover:bg-cyan-600/80"
+        >
+          <p className="text-white text-center">Generate</p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
